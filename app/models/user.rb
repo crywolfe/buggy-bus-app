a63570d5154842bab1cace7f32207511c130f185
andimrob <robert.emery1@gmail.com>
2014-04-26 20:52:35 -0400

    changed some style

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
 
   has_many :reviews
 
-  validates(:name, { presence: true })
-  validates(:email, { uniqueness: {case_sensitive: false}})
+  validates :name, presence: true
+  validates :email, uniqueness: {case_sensitive: false}
 end
