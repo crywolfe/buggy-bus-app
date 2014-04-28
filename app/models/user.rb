c053ca5a72c7b26951e098a8f41ae7d67c476483
andimrob <robert.emery1@gmail.com>
2014-04-28 10:24:21 -0400

    added simplecov gem for test coverage, added tests for user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
 
   has_many :reviews
 
-  validates :name, presence: true
+  validates :name, :email, presence: true
   validates :email, uniqueness: {case_sensitive: false}
 end
