14bc48b454fd17ea42ba8f56b1a9d579c4eaf981
crywolfe <gwolfe2007@gmail.com>
2014-04-28 16:39:41 -0400

    seeded app to ensure admin boolean was present

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,10 +1,21 @@
 var App = {
 
-  $( "#login" ).click(function() {
-    $( "#login_detail" ).slideDown( "slow", function() {
-      // Animation complete.
+  $(document.body).click(function() {
+    $("#login.detail").slideDown( "slow", function() {
+
+      alert("Slide")
+
     });
+
+
   });
+
+  // $( "#login" ).click(function() {
+  //   $( "#login_detail" ).slideDown( "slow", function() {
+  //     alert("SLIDEDOWN")
+  //     // Animation complete.
+  //   });
+  // });
 
 
 
