0f4d73571c89b2c38fdfa0cf0469e4455f79bb78
crywolfe <gwolfe2007@gmail.com>
2014-04-29 07:26:49 -0400

    moved some css to login_detail

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,10 +1,29 @@
-
   $( document ).ready(function() {
+    // EVENT HANDLER
+    // 1. CLICK ON LOGIN
     $( "#login_detail" ).hide();
     $( "#login" ).click(function () {
       $( "#login_detail" ).slideDown( "slow", function() {
         });
     });
+    // 2. CLICK SEARCH
+    $( "#search_results" ).hide();
+    $( "#search" ).click(function () {
+      $( "#search_results" ).slideDown( "slow", function() {
+        });
+    });
+
+    // 3. CLICK LUCKY
+    $( "#lucky_results" ).hide();
+    $( "#lucky" ).click(function () {
+      $( "#lucky_results" ).slideDown( "slow", function() {
+        });
+    });
+
+
+
+
+
   });
 
 
