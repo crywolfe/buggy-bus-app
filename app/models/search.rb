6ef00c0ebbd0298f8f55573e133453f07e439512
Rob <robert.emery1@gmail.com>
2014-06-07 12:32:29 -0400

    new search method to scrape from search controller and trimmed seeds file

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,19 @@
 class Search < ActiveRecord::Base
+  include MegabusScraper
+  include GotobusScraper
 
-  # def parse_date(params)
-  #   year = params["departure_date(1i)"]
-  #   month = params["departure_date(2i)"]
-  #   day = params["departure_date(3i)"]
-  #   return "#{year}/#{month}/#{day}"
-  # end
+  def collect(dep_date, dep_location, arrival_location)
+    day = Time.now.strftime("%Y/%-m/%-d")
+
+    mega = MegabusScraper.new
+    goto = GotobusScraper.new
+
+    mega.build_query(dep_date, dep_location, arrival_location)
+    goto.build_query(dep_date, dep_location, arrival_location)
+
+    unified_results = goto.parse + mega.parse
+
+    return unified_results
+  end
+
 end
