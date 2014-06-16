c731a9d2bebc480a43aae1c35dea075c625e3596
crywolfe <gwolfe2007@gmail.com>
2014-06-16 14:20:41 -0500

    re-work tests to match new functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe User do
 
-  it{ should validate_presence_of(:name) }
-  it{ should validate_presence_of(:email) }
-  it{ should validate_uniqueness_of(:email) }
+  # it{ should validate_presence_of(:name) }
+  # it{ should validate_presence_of(:email) }
+  # it{ should validate_uniqueness_of(:email) }
 
 end
